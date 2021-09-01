5a7b02a5ed425629af90e78f55da041503a30893
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2021-09-01 12:33:22 -0700

    Add project files

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", openLoginPage);
+document.querySelector(".email-address-button").addEventListener("click", openLoginPage);
+
+function openLoginPage(){
+    window.location.href="login_page/login.html";
+}
